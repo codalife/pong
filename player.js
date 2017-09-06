0998e46dcb7bbd0ec2f34002a38a599866696b98
Albert Chung <aychung86 (at) gmail.com>
2017-09-05 19:38:55 -0700

    player.js: setup class skeleton

---
+++ player.js
@@ -0,0 +1,11 @@
+var player = class {
+  constructor(username) {
+    this.username = username;
+    this.score = 0;
+    this.length = 0; // length of the bar
+    this.position = 0;  // top position of the bar
+                        //   bottom will be this.position + this.length
+  }
+
+
+}
